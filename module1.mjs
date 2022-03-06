4a757ada87c8937fe2414d11c9bbb11c2b8e376f
Ahana-Nandi-Tultul <ahananondi6@gmail.com>
2022-03-06 22:18:06 +0600

    Adding Props and PropsType

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a ="Ahana";
+const b="Argha";
+const c= "Joya";
+const d="Misti";
+
+export default d;
+export {a};
+export {b};
+export {c};
---
+++ module2.mjs
@@ -0,0 +1,4 @@
+import adz, {a,b,c} from './module1.mjs'
+console.log(adz);
+console.log(a);
+console.log(c);
